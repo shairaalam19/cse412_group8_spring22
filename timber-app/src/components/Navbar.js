98aaa8c6a99e316250726bca8604f825da0e7295
Shaira Alam <shaira.alam19@gmail.com>
2022-04-21 17:50:03 -0700

    sign-up page with nav bar modificaiton

--- timber-app/src/components/Navbar.js
+++ timber-app/src/components/Navbar.js
@@ -70,7 +70,7 @@ return (
           </li>
         </ul>
         
-        {button && <Button buttonStyle='btn--outline'> SIGN UP </Button>}
+        {button && <Button buttonStyle='btn--outline' path='/sign-up'> SIGN UP </Button>}
         </div> 
 
     </nav>
