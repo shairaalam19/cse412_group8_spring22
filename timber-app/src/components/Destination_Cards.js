0f970eace0195b64359e95d74a603147a9bb3508
Shaira Alam <shaira.alam19@gmail.com>
2022-04-25 00:20:48 -0700

    genrealized

--- timber-app/src/components/Destination_Cards.js
+++ timber-app/src/components/Destination_Cards.js
@@ -5,7 +5,21 @@ import {Button} from './Button';
 
 
 export function Destination_Cards(props) {
-  const [list, setList] = useState({});
+  const [list, setList] = useState({
+    destinations: [
+      // destination_name: '',
+      // trails: '', 
+      // accessibility: '',
+      // location: '',
+      // climate: ''
+    ]
+  }); // Destination names
+
+  // const [list, setList] = useState({
+  //   destination: [
+
+  //   ]
+  // }); // Destination names
 
   function card(destination){
     return (
@@ -13,8 +27,8 @@ export function Destination_Cards(props) {
         <CardItem
           // src="images/img-9.jpg"
           // text="Explore the hidden waterfall deep inside the Amazon Jungle"
-          title={destination}
-          trails="Trails" 
+          title={destination.destination}
+          trails="Trails"
           accessibility="Accessibility" 
           location="Location"
           climate="Climate"
@@ -27,15 +41,15 @@ export function Destination_Cards(props) {
 
   function allCards(){
     const items = [];
-
-    // list = props.destinations;
-
-    for(var i = 0; i < list.length; i++){
+    console.log("inside allCards");
+    // console.log("inside allCards: list = ");
+    console.log(list);
+    for(var i = 0; i < list.destinations.length; i++){
       items.push(
-        card(list[i])
+        card(list.destinations[i])
       );
     }
-
+    console.log(list.destinations.length);
     return (
       <>
         {items}
@@ -53,36 +67,70 @@ export function Destination_Cards(props) {
       if(sel.value=="name"){
         const results = await fetch(`http://localhost:5000/api/destinations/search/?destination_name=${searchInput}`);
         const parseResults = await results.json();
-        
-        const contents = []; 
+
+        // Setting list state 
+        const contents = {
+          destinations: []
+        }; 
         for(var i = 0; i < parseResults.destinations.length; i++){
-          contents.push(parseResults.destinations[i].destination_name);
+          contents.destinations.push({
+            destination: parseResults.destinations[i].destination_name,
+            trails: "Trails", 
+            accessibility: "accessibility",
+            location: "location",
+            climate: "climate"
+          });
         }
+        // console.log("contents: ");
+        // console.log(contents);
+
         setList(contents);
-        console.log(parseResults.destinations);
-        console.log(list);
+        
+
       }
       else if(sel.value=="state"){
         const results = await fetch(`http://localhost:5000/api/destinations/search/filter/state/?val=${searchInput}`);
         const parseResults = await results.json();
-        const contents = []; 
+
+        // Setting list state 
+        const contents = {
+          destinations: []
+        }; 
         for(var i = 0; i < parseResults.destinations.length; i++){
-          contents.push(parseResults.destinations[i].destination_name);
+          contents.destinations.push({
+            destination: parseResults.destinations[i].destination_name,
+            trails: "Trails", 
+            accessibility: "accessibility",
+            location: "location",
+            climate: "climate"
+          });
         }
+        // console.log("contents: ");
+        // console.log(contents);
+
         setList(contents);
-        console.log(parseResults.destinations);
-        console.log(list);
       }
       else{
         const results = await fetch(`http://localhost:5000/api/destinations/search/filter/zipcode/?val=${searchInput}`);
         const parseResults = await results.json();
-        const contents = []; 
+
+        // Setting list state 
+        const contents = {
+          destinations: []
+        }; 
         for(var i = 0; i < parseResults.destinations.length; i++){
-          contents.push(parseResults.destinations[i].destination_name);
+          contents.destinations.push({
+            destination: parseResults.destinations[i].destination_name,
+            trails: "Trails", 
+            accessibility: "accessibility",
+            location: "location",
+            climate: "climate"
+          });
         }
+        // console.log("contents: ");
+        // console.log(contents);
+
         setList(contents);
-        console.log(parseResults.destinations);
-        console.log(list);
       }
     }
     catch(err){
