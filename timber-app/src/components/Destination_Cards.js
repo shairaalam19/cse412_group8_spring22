5bf12fe174b81d1b1884d07a34be745b9fcc0b99
mckuletzz <gaguas@asu.edu>
2022-04-25 00:23:33 -0700

    Fixed That

--- timber-app/src/components/Destination_Cards.js
+++ timber-app/src/components/Destination_Cards.js
@@ -158,7 +158,7 @@ export function Destination_Cards(props) {
           <option value="zipcode">Zipcode</option>
         </select>
 
-                      <button id="searchbutton" onClick={searchInput} className="btn-success">Search</button>
+                      <button id="searchbutton" onClick={SearchDestination} className="btn-success">Search</button>
         
         </form>
       </div>
