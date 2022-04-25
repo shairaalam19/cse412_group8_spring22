ea3658d79c86c5ae6d01eeaa3f86f86e0e52fe60
Shaira Alam <shaira.alam19@gmail.com>
2022-04-25 13:27:12 -0700

    favorites list printing

--- timber-app/src/components/pages/User.js
+++ timber-app/src/components/pages/User.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import HikerAPI from "../../apis/HikerAPI";
 import FavoriteAPI from "../../apis/FavoriteAPI";
 import '../../App.css';
@@ -13,9 +13,7 @@ export function User(user) {
         hiker_password: "",
         hiker_state: ""
     });
-    const [favorites, setFavorites] = useState({});
-
-    const [favoriteString, setFavoriteString] = useState("");
+    const [favorites, setFavorites] = useState("");
     
     function getUserDetails(id){
         // var userID = 1; // connect to database to get user details
@@ -23,13 +21,6 @@ export function User(user) {
             try{
                 const response = await HikerAPI.get("/");
                 const users = response.data.hikers;
-                // var i = 0;
-                // for(i = 0; i < response.data.length; i++){
-                //     if(users[i] == name){
-                //         break;
-                //     }
-                // }
-
                 setUserData({
                     hiker_userid: response.data.hikers[id].hiker_userid,
                     hiker_username: response.data.hikers[id].hiker_username,
@@ -57,14 +48,27 @@ export function User(user) {
     }
 
     function getUserFavorites(id){
-        let destination = "Grand Canyon"; // connect to database to get these details (select query)
         // var userID = 1; // connect to database to get user details
         const fetchData = async() => {
             try{
                 const response = await FavoriteAPI.get("/" + id);
-                // console.log(response.data.favorites);
-                setFavorites(response.data.favorites);
-                // setFavorites(response.data.favorites);
+                var list = response.data.favorites;
+                
+                var favorites_string = [];
+                for(var i = 0; i < list.length-1; i++){
+                    favorites_string.push(
+                        <>
+                            {list[i].destination_name}<br></br>
+                        </>
+                    )
+                }
+                favorites_string.push(
+                    <>
+                        {list[list.length-1].destination_name}
+                    </>
+                )
+    
+                setFavorites(favorites_string);
             }
             catch(err){
                 console.log(err);
@@ -73,28 +77,11 @@ export function User(user) {
         }//end of fetch data
         
         fetchData();
-        function printFavorites(favorites){
-            // console.log(favorites[0].destination_name);
 
-            var favorites_string = "";
-            for(var i = 0; i < favorites.length-1; i++){
-                favorites_string = favorites_string + favorites[i].destination_name + ", ";
-                // console.log(favorites[i].destination_name);
-            }
-            favorites_string = favorites_string + favorites[favorites.length-1].destination_name;
-
-            // console.log(favorites_string);
-            // setFavoriteString(favorites_string);
-            return(
-                <>
-                    {favorites_string}
-                </>
-            )
-        }
         return(
             <>
-                <p>Destination: {favorites[0].destination_name}</p>
-                {/* <p>Destination: {printFavorites(favorites)}</p> */}
+                {/* <p>Destination: {favorites[0].destination_name}</p> */}
+                <p>{favorites}</p>
             </>
         )
     }
