104f5e5878a5e8935282754630c2fcc93a0bf110
Aira San Agustin <airadani25@gmail.com>
2022-04-25 15:52:51 -0700

    clean server files

--- timber-app/server/db.js
+++ timber-app/server/db.js
@@ -11,14 +11,3 @@ const pool = new Pool({
 });
 
 module.exports = pool;
-
-//*********Heroku database credentials (not working)***************
-
-// const pool = new Pool({
-//     user: "dqmvuesoxhhqlj",
-//     password: "6e6df05220f395dff07d25a8d65f697a51d5bc04ff5925c3491dcee7fe987ff5",
-//     host: "ec2-52-203-118-49.compute-1.amazonaws.com",
-//     database: "dem1qfscenbi0r",
-//     port: 5432
-//     //URI: postgres://dqmvuesoxhhqlj:6e6df05220f395dff07d25a8d65f697a51d5bc04ff5925c3491dcee7fe987ff5@ec2-52-203-118-49.compute-1.amazonaws.com:5432/dem1qfscenbi0r
-// });
