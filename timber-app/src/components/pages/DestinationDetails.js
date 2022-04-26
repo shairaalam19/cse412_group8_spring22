e2123c3f2a6b2dd5b4ef1c32ce47f6d3d7c8a67d
mckuletzz <gaguas@asu.edu>
2022-04-25 22:28:53 -0700

    k

---
+++ timber-app/src/components/pages/DestinationDetails.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import FavoriteAPI from "../apis/FavoriteAPI";
+import { Link, useLocation } from "react-router-dom";
+import "./Button.css";
+import Cookies from 'js-cookie';
+
+function DestinationDetails(props) {
+
+}
